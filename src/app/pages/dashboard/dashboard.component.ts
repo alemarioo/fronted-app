ebf96b172ee0d3dfad4512029b8bd96c9c35a300
mario ruano <marioruano25@gmail.com>
2022-10-20 16:00:28 -0600

    xd

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+    
+  }
+
+  public chart: any;
+
+  constructor() {
+    
   }
 
 }
